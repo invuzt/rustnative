afdcec5d27ef664e0c8e32cca57bf100c67a45cd
invuzt <invuzt@gmail.com>
2026-03-26 00:02:31 +0700

    getar

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,28 @@
-use android_activity::{AndroidApp, MainEvent, PollEvent};
+use android_activity::{AndroidApp, MainEvent, PollEvent, InputEvent};
+use log::info;
 use std::time::Duration;
 
 #[no_mangle]
 fn android_main(app: AndroidApp) {
+    // Inisialisasi logger agar kita bisa lihat pesan di adb logcat
+    android_logger::init_once(
+        android_logger::Config::default().with_max_level(log::LevelFilter::Info),
+    );
+
+    info!("Aplikasi Odfiz Native Dimulai!");
+
     loop {
         app.poll_events(Some(Duration::from_millis(16)), |event| {
             match event {
                 PollEvent::Main(MainEvent::Destroy) => {
-                    return;
+                    info!("Aplikasi ditutup oleh user.");
+                }
+                PollEvent::Input(InputEvent::MotionEvent(motion_event)) => {
+                    let action = motion_event.action();
+                    info!("Layar disentuh! Action: {:?}", action);
                 }
                 _ => {}
             }
         });
-
-        // Di sini kita tidak menggambar apa-apa dulu agar TIDAK FC.
-        // Jika ini jalan (layar hitam), berarti HP kamu butuh 
-        // inisialisasi EGL yang sangat spesifik.
     }
 }
