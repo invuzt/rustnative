27eb99abb5e6b2783f155053719c54db31b694e3
invuzt <invuzt@gmail.com>
2026-03-26 00:08:30 +0700

    fix: use callback for input events in android-activity 0.6

--- src/lib.rs
+++ src/lib.rs
@@ -8,21 +8,20 @@ fn android_main(app: AndroidApp) {
         android_logger::Config::default().with_max_level(log::LevelFilter::Info),
     );
 
-    info!("Aplikasi Odfiz Native Dimulai! Coba sentuh layarnya.");
+    info!("Odfiz Native: Siap menerima input!");
 
     loop {
-        // Kita ambil semua event yang masuk
         app.poll_events(Some(Duration::from_millis(16)), |event| {
             match event {
                 PollEvent::Main(MainEvent::Destroy) => {
                     info!("Aplikasi ditutup.");
                 }
-                // Cara baru akses input event di versi 0.6
                 PollEvent::Wake => {
-                    // Cek input secara manual dari queue
-                    let mut input_iter = app.input_events_iter().unwrap();
-                    while let Some(input_event) = input_iter.next() {
-                        info!("Ada input masuk ke HP: {:?}", input_event);
+                    // Cara yang benar untuk v0.6: pakai callback di dalam .next()
+                    if let Ok(mut input_iter) = app.input_events_iter() {
+                        while input_iter.next(|input_event| {
+                            info!("Input terdeteksi: {:?}", input_event);
+                        }) {}
                     }
                 }
                 _ => {}
